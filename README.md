872f377b6b4cbed6a0578e978727b7fe95ab92be
Wedeueis <wedeueisbraz@yahoo.com.br>
2017-04-15 17:36:12 -0300

    changed algorithm to linear regression

--- README.md
+++ README.md
@@ -4,7 +4,16 @@ Workbook for project presentation in Machine Learning.
 
 Dataset: Wine Quality (http://archive.ics.uci.edu/ml/datasets/Wine+Quality)
 
-Algorithm: Multilayer Perceptron and Decision Trees(for feature selection)
-
 Language: Python with scikitlearn
 
+Algorithm: Multilayer Perceptron and Decision Trees(for feature selection)
+
+Modificações
+- O professor pediu para usarmos regressão no lugar do MLP
+- O algoritmo do modelo foi modificado para regressão linear(por enquanto)
+
+Para fazer
+- Implementar o k-fold cross validation 
+- Implementar a seleção de atributos
+- Outros testes/algoritmos
+- Relatório
--- Regressor.py
+++ Regressor.py
@@ -1,25 +1,48 @@
 import csv
 import numpy as np
-from sklearn.neural_network import MLPRegressor
+from sklearn import linear_model
 from sklearn.preprocessing import normalize
 
-csv_file_object = csv.reader(open('winequality-red.csv'), 
-						delimiter=';', quotechar='"')
+#Lendo a base de dados de um arquivo csv
+csv_file_object = csv.reader(open('winequality-white.csv'), 
+						delimiter=',', quotechar='"')
 data = []
 for row in csv_file_object:
 	data.append(row)
 
 data = np.array(data)
 
+#Separando os atributos das etiquetas
 X = data[1:,:11]
 X = X.astype(np.float)
 Y = data[1:,11]
 Y = Y.astype(np.float)
 
-norm_x = []
-for i in X:
-	norm_x.append(normalize(i[:,np.newaxis], axis=0).ravel())
+#Normalizando a amplitude
+norm_x = X / X.max(axis=0)
 
-clf = MLPRegressor(hidden_layer_sizes=(11,), early_stopping=True)
+#Separando o conjunto de treinamento do de teste
+X_train = X[:-1200]
+X_test = X[-1200:]
 
-clf.fit(norm_x, Y)
+print('Tamanho do conjunto de treinamento: \n', len(X_train))
+print('Tamanho do conjunto de teste: \n', len(X_test))
+
+Y_train = Y[:-1200]
+Y_test = Y[-1200:]
+
+#Criando o modelo de regressão linear
+regr = linear_model.LinearRegression()
+
+#Treinando o modelo na base de treinamento
+regr.fit(X_train, Y_train)
+
+#Coeficientes da reta aprendida
+print('Coefficients: \n', regr.coef_)
+
+#Erro médio quadrático
+print("Mean squared error: %.2f"
+      % np.mean((regr.predict(X_test) - Y_test) ** 2))
+
+#Pontuação em variancia explicada: 1 é uma previsão perfeita
+print('Variance score: %.2f' % regr.score(X_test, Y_test))
